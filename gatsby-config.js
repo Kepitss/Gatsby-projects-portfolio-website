74aa044cd79a72958760692a21e0a8c1d9aa0e13
Kepitss <keper524@gmail.com>
2021-11-26 11:55:13 +0200

    build: set up gatsby-config.js
    - Create SiteMetadate
    - Add google fonts what you need for aplication
    - Add gatsby-plugin-image plugin and connect images folder to GraphQl

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    motto: `We think context matters`,
+    description: `Our work appears regularly in both mainstream and industry specific publications.`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Raleway", "Nova Flat", "Poppins"],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
