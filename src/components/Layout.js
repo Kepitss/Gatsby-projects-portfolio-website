1d429a1bd5865f80607e5371ebe9ec891b7f671a
Kepitss <keper524@gmail.com>
2021-11-29 10:33:20 +0200

    feat: create Navbar, Sidebar which wraps all pages
    - Create navigation bar(Navbar)
    - Create Sidebar which is used on small screen size when Navbar links
      are hidden to show links
    - Connect Navbar, Sidebar components to Layout component to show  these components
      in all pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 
 import "normalize.css"
 import "../styles/sass/main.scss"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
 
 const Layout = ({ children }) => {
+  const [sidebar, setSidebar] = useState(false)
   return (
     <div className="base">
-      Layout component
+      <Navbar sidebar={sidebar} setSidebar={setSidebar} />
+      <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
       {children}
     </div>
   )
