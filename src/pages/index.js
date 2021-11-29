8392a6b17072584a99719ef69bfa47cd0210bc73
Kepitss <keper524@gmail.com>
2021-11-29 11:08:13 +0200

    feat: create homepage Slider component
    - Connect slider images folder to GraphQL
    - Use GarphQL slider images folder content
    - Use SiteMetadata in Slider component
    - Connect Slider component to homepage(index.js)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,11 @@
 import React from "react"
 
+import Slider from "../components/Slider"
+
 export default function Home() {
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt non
-        accusantium dignissimos debitis molestiae dolor veritatis neque ullam
-        sunt? Ullam nobis reiciendis cumque qui temporibus eaque distinctio
-        soluta excepturi quibusdam sint laudantium explicabo natus quae ipsam
-        autem ipsa, corporis, vero dolorum quod maiores modi et saepe nulla.
-        Facere quidem, ipsum odio explicabo voluptatem assumenda accusantium
-        optio saepe, aspernatur commodi officiis at vel. Quisquam dolores
-        temporibus officiis minima. Nemo perspiciatis quos, quae eaque veritatis
-        voluptatibus quasi adipisci non dicta ratione similique in impedit
-        cupiditate, mollitia error doloremque minima? Quaerat quibusdam nemo
-        numquam? Libero ratione aspernatur quasi dolor, quibusdam id. Nulla
-        corporis error nisi laborum harum eius hic, illo impedit. Corrupti
-        corporis a unde, vel blanditiis in sint sapiente itaque, animi nisi,
-        ratione adipisci molestiae similique deleniti vero dignissimos?
-        Doloremque, explicabo odit omnis sunt magni molestias sapiente!
-        Repellendus tempore, architecto dolorem neque, esse eveniet vel rem
-        praesentium dignissimos aliquid amet soluta fugit veritatis quo ipsum
-        incidunt unde autem similique atque? Iure rerum dignissimos quidem
-        repellat! Ratione, voluptatibus ipsam? Cupiditate sit, molestiae
-        dignissimos recusandae illo harum, dolorem dolore pariatur ratione in
-        eum ut beatae vitae itaque exercitationem voluptatibus expedita officia
-        incidunt nobis laboriosam at maiores quas nesciunt ullam? Nulla
-        voluptate amet perferendis ullam iste blanditiis, dignissimos optio
-        voluptatum quae totam deleniti illum nesciunt soluta cumque? Hic
-        pariatur totam modi quo tempora excepturi, nostrum dicta sequi
-        reprehenderit? Error debitis odio non fuga velit corrupti minus earum
-        repellendus molestias neque ipsam dolor placeat similique aperiam
-        laudantium mollitia asperiores voluptatum iure fugit, optio officiis.
-        Amet, commodi.
-      </p>
-    </div>
+    <>
+      <Slider />
+    </>
   )
 }
