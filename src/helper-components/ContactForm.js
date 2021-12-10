b1edbd1b5662db41bb475daa4ba82d015ba325ba
Kepitss <keper524@gmail.com>
2021-12-10 19:46:00 +0200

    refactor: ContactForm component to helper
    - Create ContactForm component to be helper component
    - Use ContactForm component right in components which uses it

--- src/helper-components/ContactForm.js
+++ src/helper-components/ContactForm.js
@@ -1,12 +1,10 @@
 import React from "react"
-import { useForm, ValidationError } from "@formspree/react"
+import { ValidationError } from "@formspree/react"
 
 import Title from "./Title"
 
-export default function ContactForm() {
-  const [state, handleSubmit] = useForm(process.env.GATSBY_CONTACT_FORM)
-
-  if (state.succeeded) {
+export default function ContactForm({ title, submit, handleSubmit }) {
+  if (submit.succeeded) {
     return (
       <div className="success-msg">
         <h3>Thanks for your submission!</h3>
@@ -16,24 +14,24 @@ export default function ContactForm() {
 
   return (
     <div className="contact-form-container">
-      <Title title="contact us" />
+      <Title title={title} />
       <form onSubmit={handleSubmit} className="contact-form">
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="Email..." />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <ValidationError prefix="Email" field="email" errors={submit.errors} />
 
         <label htmlFor="message">Message:</label>
         <textarea id="message" name="message" placeholder="Message..." />
         <ValidationError
           prefix="Message"
           field="message"
-          errors={state.errors}
+          errors={submit.errors}
         />
 
-        <button type="submit" disabled={state.submitting}>
+        <button type="submit" disabled={submit.submitting}>
           Submit
         </button>
-        <ValidationError className="validator" errors={state.errors} />
+        <ValidationError className="validator" errors={submit.errors} />
       </form>
     </div>
   )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { useForm } from "@formspree/react"
 
 import ContactForm from "../helper-components/ContactForm"
 
 const Contact = () => {
+  const [submit, handleSubmit] = useForm(process.env.GATSBY_CONTACT_FORM)
   return (
     <>
-      <ContactForm />
+      <ContactForm
+        title="contact us"
+        submit={submit}
+        handleSubmit={handleSubmit}
+      />
     </>
   )
 }
