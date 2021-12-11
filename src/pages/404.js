aff786212fa407a0f07ec472c3b9d8bf7a396b0a
Kepitss <keper524@gmail.com>
2021-12-11 22:16:19 +0200

    feat: create good 404 page
    - 404 page is now better looking and with button to go back to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Error = () => {
-  return <div>Error page</div>
+  return (
+    <div className="error-page-container">
+      <h1>Sorry but the page does not exist</h1>
+      <Link to="/" className="home-btn">
+        Return home
+      </Link>
+    </div>
+  )
 }
 
 export default Error
