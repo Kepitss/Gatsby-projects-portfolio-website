e219ada9b2682eec09fd0b76dac6522fa0972f0e
Kepitss <keper524@gmail.com>
2021-12-13 11:55:06 +0200

    build: add data for seo
    - Add SiteMetadata for SEO
    - Add favicon and main-img in static folder for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,13 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
+    title: "Mārtiņš Ķepīts",
     motto: `We think context matters`,
     description: `Our work appears regularly in both mainstream and industry specific publications.`,
+    titleTemplate: "%s | Gatsby Project",
+    url: "https://martinskepits.lv/", // No trailing slash allowed!
+    image: "/main-img.png", // Path to the image placed in the 'static' folder, in the project's root directory.
+    twitterUsername: "@kepits_martins",
   },
   plugins: [
     `gatsby-plugin-image`,
